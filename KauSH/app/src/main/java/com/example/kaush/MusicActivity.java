6f82bda152a41ad5a4826952bac10edb14f18efd
qmdl980 <qmdl98056@gmail.com>
2020-05-02 21:06:58 +0900

    200502HJ

--- KauSH/app/src/main/java/com/example/kaush/MusicActivity.java
+++ KauSH/app/src/main/java/com/example/kaush/MusicActivity.java
@@ -14,6 +14,8 @@ public class MusicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music);
 
         TextView musicEmotionText = findViewById(R.id.text_music_emotion);
+        TextView musicTitleText = findViewById(R.id.text_music_title);
+        TextView musicDateText = findViewById(R.id.text_music_date);
         String textEmotion = "n";
         Intent intent = getIntent();
 
@@ -22,5 +24,22 @@ public class MusicActivity extends AppCompatActivity {
         if(textEmotion.equals("love")){ musicEmotionText.setText("같이 이 노래 들어보는거 어때요?"); }
         else if(textEmotion.equals("solace")){ musicEmotionText.setText("이 노래 들으면 위로가 될거에요"); }
         else if(textEmotion.equals("cry")){ musicEmotionText.setText("펑펑 울고 털어내세요"); }
+
+        MusicInfo sampleMusic = new MusicInfo("조이 - 좋은사람있으면소개시켜줘", "2016-4-2", "https://www.youtube.com/watch?v=hoLzH1revMg");
+
+        musicTitleText.setText(sampleMusic.title);
+        musicDateText.setText(sampleMusic.date);
+    }
+
+    public void MusicLove(){
+
+    }
+
+    public void MusicSolace(){
+
+    }
+
+    public void MusicCry(){
+
     }
 }
---
+++ KauSH/app/src/main/java/com/example/kaush/MusicInfo.java
@@ -0,0 +1,13 @@
+package com.example.kaush;
+
+public class MusicInfo {
+    String title;
+    String date;
+    String uri;
+
+    public MusicInfo(String title, String date, String uri){
+        this.title = title;
+        this.date = date;
+        this.uri = uri;
+    }
+}
