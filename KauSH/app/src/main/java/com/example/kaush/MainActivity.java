5409118e278699786310a521dd0aed452db12c99
jjmmll0727 <48875473+jjmmll0727@users.noreply.github.com>
2020-05-08 21:21:48 +0900

    jm

--- KauSH/app/src/main/java/com/example/kaush/MainActivity.java
+++ KauSH/app/src/main/java/com/example/kaush/MainActivity.java
@@ -8,7 +8,10 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -46,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
     public String text_data = "입력받지 않음"; // 사용자가 말하는 음성데이터 **
     FileOutputStream outputStream; // 파일 입출력을 위한 파일객체 **
     private TransferUtility transferUtility;
+    String probability_list;
+    String[] probabilities = new String[3]; // 확률값들만 저장하는 배열
 
 
     @Override
@@ -69,6 +74,23 @@ public class MainActivity extends AppCompatActivity {
 
         s3.setRegion(Region.getRegion(Regions.US_EAST_2));
         s3.setEndpoint("s3.us-east-2.amazonaws.com");
+
+
+        new Thread() {
+            public void run() {
+                String nodingHtml = getNodingHtml();
+
+                Bundle bun = new Bundle();
+                bun.putString("NODING_HTML", nodingHtml);
+                Message msg = handler.obtainMessage();
+                msg.setData(bun);
+                handler.sendMessage(msg);
+            }
+        }.start();
+
+        //tvNaverHtml = (TextView)this.findViewById(R.id.tv_naver_html);
+
+        //출처: https://docko.tistory.com/entry/안드로이드-androidosNetworkOnMainThreadException [장똘]
 
         // 시작하자마자 녹음하기
         /*if(isConnected()){
@@ -127,6 +149,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private String getNodingHtml(){
+        String nodingHtml = "";
+
+        URL url =null;
+        HttpURLConnection http = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+
+        try{
+            url = new URL("https://43sisn313b.execute-api.us-east-2.amazonaws.com/sage-function");
+            http = (HttpURLConnection) url.openConnection();
+            http.setConnectTimeout(3*1000);
+            http.setReadTimeout(3*1000);
+
+            isr = new InputStreamReader(http.getInputStream());
+            br = new BufferedReader(isr);
+
+            String str = null;
+            while ((str = br.readLine()) != null) {
+                nodingHtml += str + "\n";
+            }
+
+        }catch(Exception e){
+            Log.e("Exception", e.toString());
+        }finally{
+            if(http != null){
+                try{http.disconnect();}catch(Exception e){}
+            }
+
+            if(isr != null){
+                try{isr.close();}catch(Exception e){}
+            }
+
+            if(br != null){
+                try{br.close();}catch(Exception e){}
+            }
+        }
+        return nodingHtml;
+    }
+
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            Bundle bun = msg.getData();
+            String nodingHtml = bun.getString("NODING_HTML");
+            probability_list = nodingHtml;
+            //tvNaverHtml.setText(naverHtml);
+            //System.out.println("|||||||||||| " + nodingHtml);
+            dividedProbability();
+
+        }
+    };
+    //출처: https://docko.tistory.com/entry/안드로이드-androidosNetworkOnMainThreadException [장똘]
+
+    // [] 를 포함한 확률 리스트에서 확률값들만 뽑아내는 함수
+    private void dividedProbability()
+    {
+        //probability_list.substring(0, probability_list.length()-2);
+        //probabilities = probability_list.substring(1, probability_list.length()-2).split(",");
+        probabilities = probability_list.split(",");
+        for(int i=0; i<3; i++)
+        {
+            System.out.println("probabilities: "+probabilities[i]);
+        }
+        //System.out.println("====================" + probability_list.substring(1, probability_list.length()-2));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
@@ -169,10 +257,26 @@ public class MainActivity extends AppCompatActivity {
                     "myfile.txt",
                     new File("/data/data/com.example.kaush/files/myfile")
             );
+            /*
+            // url 읽어오기
+            String url = "";
+            InputStream is = null;
+            try {
+                is = new URL("https://43sisn313b.execute-api.us-east-2.amazonaws.com/sage-function").openStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                String str;
+                StringBuffer buffer = new StringBuffer();
+                while ((str = rd.readLine()) != null) {
+                    buffer.append(str);
+                }
+                String receiveMsg = buffer.toString();
+                System.out.println("||||||||||||||||||" + receiveMsg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //출처: https://bottlecok.tistory.com/52 [잡캐의 IT 꿀팁]
+            */
 
-            // URL 에서 데이터 읽어오기
-            // 읽어올 url : https://43sisn313b.execute-api.us-east-2.amazonaws.com/sage-function
-            // 여기까지
         }
         super.onActivityResult(requestCode, resultCode, data);
 
